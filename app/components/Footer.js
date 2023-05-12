e02e20434f86c84886e2cd41f0098b1c9da737fd
kgb <kester4000@gmail.com>
2023-05-12 07:15:10 -0400

    First code commit.

---
+++ app/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <p>&copy;{new Date().getFullYear()} Supercar Rentals Las Vegas.</p>
+      </footer>
+    </>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,95 +1,229 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Head from 'next/head'
+import Footer from './components/Footer'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      
+      <header></header>
+      <main>
+        <div className="container">
+          <h1>Rent a McLaren in Las Vegas</h1>
+          <img
+            src="/mclaren-570GT.webp"
+            alt="Rent a McLaren in Las Vegas."
+            title="Rent a McLaren in Las Vegas"
+          />
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+        <div className="container">
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            At Supercar Rentals Las Vegas, we turn dreams into reality by
+            offering you the most luxurious and thrilling McLaren rental
+            experience in fabulous Las Vegas.
           </p>
-        </a>
-      </div>
-    </main>
+          <p>
+            Our mission is to provide you with an unforgettable driving
+            adventure that will elevate your Vegas experience to new heights.
+          </p>
+          <p>
+            Whether you're cruising the Strip, exploring the surrounding scenic
+            roads, or attending a glamorous event, our fleet of exotic cars is
+            here to make your stay in Sin City one for the books.
+          </p>
+          <button>Reserve Your McLaren Today!</button>
+          <h2>Our Exquisite Fleet</h2>
+          <p>
+            Supercar Rentals Las Vegas is proud to present two of the world's
+            most stunning and high-performance exotic cars. The McLaren 570GT
+            and the McLaren 570S.
+          </p>
+          <div className="cars-for-rent">
+            <div className="cars">
+              <h3>McLaren 570GT - Elegance Meets Power</h3>
+              <img
+                src="/mclaren-570GT.webp"
+                alt="Rent a McLaren 570GT in Las Vegas."
+                title="Rent a McLaren 570GT in Las Vegas."
+              />
+              <p>
+                Experience the perfect blend of luxury, comfort, and sportiness
+                with our McLaren 570GT. Born from the McLaren Sports Series
+                family, the 570GT is the ultimate sports car experience, with a
+                touch of grand tourer luxury.
+              </p>
+              <div className="car-btns">
+                <button>Reserve Now!</button>
+                <button>Learn More!</button>
+              </div>
+            </div>
+            <div className="cars">
+              <h3>McLaren 570S - Sportiness Redefined</h3>
+              <img
+                src="/mclaren-570S.webp"
+                alt="Rent a McLaren 570S in Las Vegas."
+                title="Rent a McLaren 570S in Las Vegas."
+              />
+              <p>
+                Enter the world of pure exhilaration with the McLaren 570S. This
+                elegant beast from the McLaren Sports Series is a testament to
+                the British automaker's racing heritage, offering an
+                uncompromised driving experience that combines thrilling
+                performance, sophisticated design, and advanced technology.
+              </p>
+              <div className="car-btns">
+                <button>Reserve Now!</button>
+                <button>Learn More!</button>
+              </div>
+            </div>
+          </div>
+          <p>
+            Our McLarens are meticulously maintained to ensure your safety,
+            comfort, and enjoyment. Each vehicle is detailed to perfection and
+            equipped with state-of-the-art technology, allowing you to
+            experience the full potential of these engineering marvels.
+          </p>
+          <h2>Why Choose Supercar Rentals Las Vegas</h2>
+          <p>
+            Although there are many exotic car rental companies in Las Vegas,
+            there are reasons to get your car form us.
+          </p>
+          <div className="faq">
+            <h3>#1. Unrivalled Fleet</h3>
+            <p>
+              Our collection comprises the most sought-after models, from the
+              exhilarating McLaren 570S to the luxurious McLaren 570GT.
+            </p>
+            <h3>#2. Reservations Made Easy</h3>
+            <p>
+              Booking your dream car has never been easier. Our user-friendly
+              website lets you browse through our impressive inventory and
+              reserve your chosen vehicle with just a few clicks. Our
+              knowledgeable team is also available to assist you via phone,
+              email, or live chat if you have any questions or require
+              personalized recommendations.
+            </p>
+            <h3>#3. Premium Service</h3>
+            <p>
+              We pride ourselves on offering personalized, top-notch service.
+              Our team of dedicated professionals are committed to ensuring your
+              rental experience is seamless and memorable.
+            </p>
+            <h3>#4. Fair Pricing</h3>
+            <p>
+              We believe that luxury should be accessible. That's why we offer
+              competitive rates, special promotions, and flexible rental terms.
+            </p>
+            <h3>#5. Safety First</h3>
+            <p>
+              Your safety is our top priority. All our cars undergo rigorous
+              maintenance checks and are equipped with advanced safety features.
+            </p>
+          </div>
+          <button>Call today to reserve your McLaren!</button>
+          <h2>About Us</h2>
+          <p>
+            Supercar Rentals Las Vegas is your premier destination for
+            experiencing the thrill and luxury of the world's most exquisite
+            supercars. Since our establishment in 2022, we've been offering the
+            opportunity to drive the dream in the heart of Las Vegas, the city
+            known for its glamour, entertainment, and thrill.
+          </p>
+          <p>
+            Our love for supercars is deeply rooted in our team's shared passion
+            for power, performance, and precision engineering. We understand the
+            allure of these incredible machines and we take immense pleasure in
+            sharing this excitement with our customers.
+          </p>
+          <p>
+            Our commitment goes beyond just providing exceptional vehicles. We
+            strive to deliver an unrivaled customer experience, marked by
+            personalized service, transparent pricing, and absolute convenience.
+            We meticulously maintain our fleet to ensure that every car we rent
+            out is in impeccable condition, both aesthetically and mechanically.
+          </p>
+          <p>
+            At Supercar Rentals Las Vegas, we believe in transforming journeys
+            into unforgettable experiences. Our mission is to help you create
+            memories filled with the roar of powerful engines, the thrill of
+            speed, and the luxury of the world’s finest automobiles.
+          </p>
+          <button>Call today to reserve your McLaren!</button>
+          <h2>Frequently Asked Questions</h2>
+          <div className="faq">
+            <h3>Who can rent a McLaren?</h3>
+            <p>
+              Any licensed driver aged 25 or older with full-coverage auto
+              insurance can rent a McLaren from us.
+            </p>
+            <h3>What do I need to bring to rent a McLaren?</h3>
+            <p>
+              You will need a valid driver's license, proof of full-coverage
+              auto insurance, and a credit card for security deposit.
+            </p>
+            <h3>Can I drive the McLaren outside of Las Vegas?</h3>
+            <p>
+              Yes, you are free to drive anywhere within the continental United
+              States. However, additional fees may apply for significant
+              mileage.
+            </p>
+            <h3>What happens if I damage the car?</h3>
+            <p>
+              In the unfortunate event of damage, you will be responsible for
+              the deductible on your insurance policy. Additional charges may
+              apply depending on the extent of the damage.
+            </p>
+            <h3>Can I extend my rental period?</h3>
+            <p>
+              Yes, subject to availability. Please inform us at least 24 hours
+              before your rental period ends if you wish to extend.
+            </p>
+            <h3>What are your operating hours?</h3>
+            <p>
+              Our office is open from 9 AM to 6 PM, seven days a week. However,
+              we offer 24-hour customer service for any inquiries or assistance
+              you might need during your rental period.
+            </p>
+            <h3>Can someone else drive the car that I rent?</h3>
+            <p>
+              Yes, additional drivers are allowed but they must meet the same
+              requirements as the main driver, including age, driving license,
+              and insurance coverage. Please note that additional charges may
+              apply.
+            </p>
+            <h3>Do I need to refuel the car before returning it?</h3>
+            <p>
+              Yes, we ask that you return the car with the same fuel level as
+              when you picked it up. If the car is returned with less fuel, a
+              refueling charge will apply.
+            </p>
+            <h3>Do you provide pick-up and drop-off services?</h3>
+            <p>
+              Yes, we do. We offer delivery and pick-up services within Las
+              Vegas. Please contact us for further details and any additional
+              fees.
+            </p>
+            <h3>Can I cancel or modify my reservation?</h3>
+            <p>
+              Yes, you can cancel or modify your reservation. However, please
+              note that cancellations or modifications must be made at least 48
+              hours before the scheduled rental time to avoid any charges.
+            </p>
+            <h3>Are the cars smoke-free?</h3>
+            <p>
+              Yes, all our vehicles are smoke-free. A cleaning fee will be
+              charged if the vehicle is returned with evidence of smoking.
+            </p>
+            <h3>Are pets allowed in the vehicles?</h3>
+            <p>
+              No, pets are not allowed in our vehicles. We strive to maintain
+              the pristine condition of our fleet for all customers to enjoy.
+            </p>
+            <button>Reserve Your McLaren Today!</button>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
   )
 }
